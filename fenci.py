18b37dbaeb1d14b876e1473e3b165c332bf6e374
icecreamtoo <2249253421@qq.com>
2017-05-22 18:59:35 +0800

    完成分词
    更新内容：去除停用词

--- fenci.py
+++ fenci.py
@@ -1,14 +1,17 @@
 #encoding=utf-8
-import jieba.analyse
-import re
+import jieba.analyse  #导入结巴分词包
+with open('stopword.txt', 'r') as f:
+    stopwords = f.read()  # 打开stopword（停用词）文件
 with open('icedoc.txt', 'r') as f:
-    #print(f.readlines())
-    w = f.read()
-seg_list = jieba.cut_for_search(w)
-x= "/".join(seg_list)
-# y = re.sub(u"[\uFF0C]","/",x)
+    w = f.read()  #读取要分词的文件
+    seg1 = jieba.cut_for_search(w)  #进行分词
+    final = []  # 设置一个变量
+    for seg_list in seg1:
+        if seg_list not in stopwords:
+            final.append(seg_list)  # 对第一次分词的结果进行处理，去掉停用词
+seg_final=" ".join(final)  #用空格符号分隔词语
 with open('icedoc1.txt', 'w') as f:
-    f.write(x)
-# stoplist = {}.fromkeys([ line.strip() for line in open("stopword.txt") ])
-#stoplist = codecs.open('stopword.txt','r',encoding='utf8').readlines()如果文件不是utf8编码
+    f.write(seg_final)#将最终的分词结果写进另外文档
+
+
 
